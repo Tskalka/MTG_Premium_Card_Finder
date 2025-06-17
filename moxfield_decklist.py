113623da3908f3320a3d6c5847a480f781283c12
Tskalka <tyler.skalka@gmail.com>
2025-06-17 10:18:08 -0600

    read JSON into a new file from moxfield's API

--- moxfield_decklist.py
+++ moxfield_decklist.py
@@ -1,6 +1,7 @@
 import cloudscraper
 import time
 import random
+import json
 
 def wait():
     time.sleep(random.uniform(0.05, 0.1))
@@ -11,8 +12,16 @@ def get_decklist(deck_list):
     wait()
     scraper = cloudscraper.create_scraper()  # This handles cookies and JavaScript
     response = scraper.get(f"https://api.moxfield.com/v2/decks/all/{deck_list}")
-    return response.text
+    return response.json()
 
-print(get_decklist(deck_list))
 
-#Now we can start iterating through the decklist to then search for the premium cards
+
+# pretty = json.dumps(get_decklist(deck_list), indent=4)
+# print(pretty)
+
+#Read JSON to file
+# Uncomment the following lines to save the output to a file
+
+# with open('output.json', 'w', encoding='utf-8') as f:
+#     json.dump(get_decklist(deck_list), f, indent=4, ensure_ascii=False)
+
